6be0cd18ba662ec15d956b05d79bc1b23abcb296
iiris_palviainen <iiris.palviainen@tuni.fi>
2021-12-16 17:37:37 +0200

    mergesortt

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -1,3 +1,4 @@
+def debug_print(debug_msg=None, **kwargs):
         print("{}: {}".format(key, value))
 
 
@@ -38,9 +39,10 @@ def merge(left, right):
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
 
-            quit(1)
+if __name__ == "__main__":
+        quit(1)
 
-    debug_print(value_list=value_list)
+debug_print(value_list=value_list)
 
-    sorted_list = mergesort(value_list)
-    print(sorted_list)
+sorted_list = mergesort(value_list)
+print(sorted_list)
